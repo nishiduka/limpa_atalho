47546aff13936c53e623c74e3acc34d57686fcd7
nishiduka <thomas.nishiduka@gmail.com>
2017-06-19 14:31:20 -0300

    First Commit

---
+++ limpa virus/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace limpa_virus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_diretorio = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pic_status = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_status)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Selecionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(101, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 92);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_diretorio);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(15, 127);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(488, 95);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selecione o Pen-Drive";
+            // 
+            // txt_diretorio
+            // 
+            this.txt_diretorio.Location = new System.Drawing.Point(17, 40);
+            this.txt_diretorio.Name = "txt_diretorio";
+            this.txt_diretorio.Size = new System.Drawing.Size(318, 29);
+            this.txt_diretorio.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(207, 248);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pic_status
+            // 
+            this.pic_status.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_status.Location = new System.Drawing.Point(42, 19);
+            this.pic_status.Name = "pic_status";
+            this.pic_status.Size = new System.Drawing.Size(84, 84);
+            this.pic_status.TabIndex = 4;
+            this.pic_status.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pic_status);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(345, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(158, 109);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(515, 315);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Limpa PenDrive";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_status)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_diretorio;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pic_status;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ limpa virus/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace limpa_virus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pic_status.BackgroundImage = limpa_virus.Properties.Resources.ok;
+        }
+        public string diretorio="vazio";
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd1 = new FolderBrowserDialog();
+            fbd1.Description = "Selecione uma pasta para realizar o Backup";
+            fbd1.RootFolder = Environment.SpecialFolder.MyComputer;
+            fbd1.ShowNewFolderButton = true;
+            fbd1.ShowDialog();
+            diretorio = fbd1.SelectedPath.ToString();
+            txt_diretorio.Text = diretorio;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string teste = MessageBox.Show("Deseja limpar o vírus do Pendrive?", "Tem certeza?", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation).ToString();
+            
+            if (teste != "OK") 
+                return;
+
+            if (diretorio != "vazio")
+            {
+                pic_status.BackgroundImage = limpa_virus.Properties.Resources.update;
+                //Process.Start("attrib", "-h -r -s /s /d" + diretorio + "*. *");
+                pic_status.BackgroundImage = limpa_virus.Properties.Resources.ok;
+                MessageBox.Show("Seu Pendrive foi limpo", "Pronto", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else 
+            {
+                pic_status.BackgroundImage = limpa_virus.Properties.Resources.erro;
+                MessageBox.Show("Ocorreu um erro, verifique se você selecionou um Pendrive", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+             }
+
+            string antigo = diretorio + ' ';
+            string novo = diretorio + "arquivos";
+            try
+            {
+                DirectoryInfo pasta = new DirectoryInfo(@diretorio);
+                Directory.Move(antigo, novo);
+            }
+            catch 
+            { 
+            }
+        }
+    }
+}
